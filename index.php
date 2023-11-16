152cc997759d75e30f61dc88c01a378c877b6bc8
firdaus aminnulloh <firdaus_23510027@stimata.ac.id>
2023-11-16 21:14:47 +0700

    Menambahkan contoh operator assignment

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    $y = 10;
+    $y += 5;
+    echo "Nilai setelah ditambah: " . $y . "<br>"; // Output 1
+    
+    $y /= 2;
+    echo "Nilai setelah dibagi: " . $y . "<br>"; // Output 2
+    
+    $y %= 3;
+    echo "Nilai setelah dimodulus: " . $y . "<br>"; // Output 3
+    
+    $text = "Hello ";
+    $text .= "World!";
+    echo $text . "<br>"; // Output 4
+    
+    $arr = [1, 2, 3];
+    $arr[] = 4;
+    print_r($arr); // Output 5
+?>
